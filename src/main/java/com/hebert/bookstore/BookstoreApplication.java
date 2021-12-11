33026360902f0031a254728a0e03fd24e3f16152
hebertsanches <hebert.sanches96@gmail.com>
2021-12-11 18:58:01 -0300

    Instancia de forma automatica da base de dados

--- src/main/java/com/hebert/bookstore/BookstoreApplication.java
+++ src/main/java/com/hebert/bookstore/BookstoreApplication.java
@@ -1,13 +1,42 @@
 package com.hebert.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApplication {
+import com.hebert.bookstore.domain.Categoria;
+import com.hebert.bookstore.domain.Livro;
+import com.hebert.bookstore.repositories.CategoriaRepository;
+import com.hebert.bookstore.repositories.LivroRepository;
 
+@SpringBootApplication
+public class BookstoreApplication implements CommandLineRunner{
+	//instacia de categoria repository e livrorepository
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica", "Livro de TI");
+		
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Loren ipsun", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+	}
+	
+	
 
 }
